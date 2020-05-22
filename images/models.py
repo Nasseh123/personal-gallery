4f78bdde43a8d1eb950a17f1615fc9f63230c4db
Manasseh Bundi <Manassehbundi2017@gmail.com>
2020-05-22 17:01:12 +0300

    Made models for the app and made migrations of the models to the database.

--- images/models.py
+++ images/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class image(models.Model):
+    image=models.ImageField(upload_to='articles/')
+    image_name=models.CharField(max_length=30)
+    image_description=models.TextField()
+    pub_date=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.image_name
+
+    class Meta:
+        ordering=['pub_date']   
+
+class location(models.Model):
+    location_name=models.CharField(max_length=60)
+
+class category(models.Model):
+    category_name=models.CharField(max_length=60)
