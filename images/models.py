6ed07a8bed7e8b44abae6eaf468b2686ed5b3260
Manasseh Bundi <Manassehbundi2017@gmail.com>
2020-05-23 08:11:42 +0300

    Made models for the app and defined the fk to the various tables.Made a one to many relationship beteen the images and location and images and category

--- images/models.py
+++ images/models.py
@@ -1,20 +1,33 @@
 from django.db import models
+import datetime as dt 
 
 # Create your models here.
+class location(models.Model):
+    location_name=models.CharField(max_length=60)
+    def __str__(self):
+        return self.location_name
+class category(models.Model):
+    category_name=models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.category_name
+
 class image(models.Model):
     image=models.ImageField(upload_to='articles/')
     image_name=models.CharField(max_length=30)
     image_description=models.TextField()
     pub_date=models.DateTimeField(auto_now_add=True)
-    
+    location=models.ForeignKey(location,on_delete=models.CASCADE)
+    category=models.ForeignKey(category,on_delete=models.CASCADE)
+
     def __str__(self):
         return self.image_name
 
     class Meta:
         ordering=['pub_date']   
 
-class location(models.Model):
-    location_name=models.CharField(max_length=60)
 
-class category(models.Model):
-    category_name=models.CharField(max_length=60)
+
+
+  
+
--- images/tests.py
+++ images/tests.py
@@ -1,3 +1,3 @@
 from django.test import TestCase
-
+from .models import image,location,category
 # Create your tests here.
